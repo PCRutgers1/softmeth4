7ddd5a67b5b05de92953c2a836d47c1444c212bc
peter <pc764@rutgers.edu>
2022-11-14 18:37:33 -0500

    chicago UI

--- src/main/java/com/softmeth/pizzeria/MainController.java
+++ src/main/java/com/softmeth/pizzeria/MainController.java
@@ -2,13 +2,41 @@ package com.softmeth.pizzeria;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainController {
 
 
     @FXML
     void orderChicagoPizza(ActionEvent e){
-        System.out.println("HI");
+        System.out.println("Chicago");
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource("ChicagoView.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+            Stage stage = new Stage();
+            stage.setTitle("Order Chicago Style Pizza");
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception exp){
+
+        }
+    }
+    @FXML
+    void orderNYPizza(ActionEvent e){
+        FXMLLoader fxmlLoader = new FXMLLoader(PizzaStoreMain.class.getResource("MainView.fxml"));
+
+    }
+    @FXML
+    void viewCurrentOrder(ActionEvent e){
+        FXMLLoader fxmlLoader = new FXMLLoader(PizzaStoreMain.class.getResource("MainView.fxml"));
+
+    }
+    @FXML
+    void viewStoreOrders(ActionEvent e){
+        FXMLLoader fxmlLoader = new FXMLLoader(PizzaStoreMain.class.getResource("MainView.fxml"));
+
     }
 }
