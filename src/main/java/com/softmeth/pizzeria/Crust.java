ef1ead194b31078f2475349e0e2479a822765ad8
JOHNJOHN0729 <114427168+JOHNJOHN0729@users.noreply.github.com>
2022-11-14 18:31:15 -0500

    Update Crust.java

--- src/main/java/com/softmeth/pizzeria/Crust.java
+++ src/main/java/com/softmeth/pizzeria/Crust.java
@@ -1,14 +1,18 @@
 package com.softmeth.pizzeria;
 
 public enum Crust {
-    BROOKLYN,
-    THIN,
-    HAND_TOSSED,
-    PAN,
-    STUFFED,
-    DEEP_DISH;
+    BROOKLYN("Brooklyn"), THIN("Thin"), HAND_TOSSED("Hand Tossed"), PAN("Pan"),
+    STUFFED("Stuffed"), DEEP_DISH("Deep Dish"), INVALID("None");
+    private final String crust;
+    private Crust(String crust) {
+        this.crust = crust;
+    }
+    public static Crust findCrust(String crust) {
+        Crust[] allTime = Crust.values();
 
-    private Crust(){
-
+        for (Crust c : allTime) {
+            if (c.name().equalsIgnoreCase(crust)) return c;
+        }
+        return INVALID;
     }
 }
